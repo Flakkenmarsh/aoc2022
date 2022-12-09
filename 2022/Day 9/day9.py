9477925aec7337becf093d58daabee3eda845e60
Marélette van Dalen <flakkenmarsh@gmail.com>
2022-12-09 19:46:15 +0100

    Update day9.py

--- 2022/Day 9/day9.py
+++ 2022/Day 9/day9.py
@@ -1,17 +1,19 @@
-import math
+import time
+
 file = open('input.csv', 'r')
 lines = file.readlines()
 lines = [line.strip('\n' ',') for line in lines]
+padding = 0
 
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, name):
         self.x = x
         self.y = y
+        self.name = name
 
 
 def print_map(map0, head, tail):
-    return
     for i in range(len(map0)):
         line = ""
         for j in range(len(map0[0])):
@@ -25,6 +27,25 @@ def print_map(map0, head, tail):
                 line += "."
         print(line)
     print("")
+
+
+def print_map_2(rope, height, width):
+    global padding
+
+    window_size = 10
+
+    temp = [["." for _ in range(height + padding)] for _ in range(width + padding)]
+    for i in range(len(rope) - 1, -1, -1):
+        print(i)
+        knot = rope[i]
+        temp[knot.x][knot.y] = knot.name
+
+    for i in range(rope[0].x - window_size, rope[0].x + window_size):
+        print("".join(temp[i][rope[0].y - window_size:rope[0].y + window_size]))
+
+    print("\n"*(10))
+    print("")
+    time.sleep(0.1)
 
 
 def ends_touch(head, tail):
@@ -62,11 +83,32 @@ def move_tail(map0, head, tail):
     return tail
 
 
+def move_tail_2(map0, head, tail):
+    if ends_touch(head, tail):
+        return tail
+
+    if head.x == tail.x and abs(head.y - tail.y) == 2:
+        diff = get_move_diff(head.y - tail.y)
+        tail.y += diff
+    elif head.y == tail.y and abs(head.x - tail.x) == 2:
+        diff = get_move_diff(head.x - tail.x)
+        tail.x += diff
+    else:
+        # diagonal
+        diff_x = get_move_diff(head.x - tail.x)
+        diff_y = get_move_diff(head.y - tail.y)
+        tail.x += diff_x
+        tail.y += diff_y
+
+    return tail
+
+
+
 def part1():
     width = 0
     height = 0
     start = Point(0, 0)
-
+    time.sleep(10)
     for line in lines:
         values = line.split(" ")
         if values[0] == 'R' or values[0] == 'L':
@@ -108,6 +150,7 @@ def part1():
             print("===========")
             # print_map(visited, head, tail)
 
+    visited[tail.x][tail.y] = "#"
     visits = 0
     for row in visited:
         visits += row.count("#")
@@ -116,10 +159,67 @@ def part1():
 
 
 def part2():
+    global padding
+    x = y = 0
+    left = right = top = bottom = 0
+
     for line in lines:
-        pass
+        values = line.split(" ")
+        if values[0] == 'R' or values[0] == 'L':
+            if values[0] == 'R':
+                x += int(values[1])
+            else:
+                x -= int(values[1])
+
+            if x >= right:
+                right = x
+            elif x <= left:
+                left = x
+        else:
+            if values[0] == 'D':
+                y += int(values[1])
+            else:
+                y -= int(values[1])
+
+            if y <= top:
+                top = y
+            elif y >= bottom:
+                bottom = y
+
+    start = Point(abs(left), abs(top), "H")
+    width = abs(right) + abs(left)
+    height = abs(top) + abs(bottom)
+    rope = [Point(start.x, start.y, str(i)) for i in range(10)]
+
+    map0 = [["." for _ in range(height + padding)] for _ in range(width + padding)]
+    visited = [["." for _ in range(height + padding)] for _ in range(width + padding)]
+    visited[rope[-1].x][rope[-1].y] = "#"
+    for line in lines:
+        values = line.split(" ")
+        for i in range(int(values[1])):
+            if values[0] == "U" or values[0] == "D":
+                rope[0].x += 1 if values[0] == "D" else -1
+
+            elif values[0] == "R":
+                rope[0].y += 1
+            else:
+                rope[0].y -= 1
+
+            for j in range(len(rope)-1):
+                rope[j+1] = move_tail_2(map0, rope[j], rope[j+1])
+                print_map_2(rope, height, width)
+            visited[rope[-1].x][rope[-1].y] = "#"
+            
+    visits = 0
+    for row in visited:
+        visits += row.count("#")
+        print("".join(row))
+    
+    for knot in rope:
+        print(knot.name, ": ", knot.x, ",", knot.y)
+    print(visits)
 
 
 if __name__ == "__main__":
-    part1()
+    # part1()
     part2()
