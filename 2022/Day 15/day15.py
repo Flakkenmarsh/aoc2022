fd75bd18edf4fe7d3ad4c49ad6413bfbe2bebeb3
Marélette van Dalen <flakkenmarsh@gmail.com>
2022-12-15 17:12:50 +0100

    updated day 15

--- 2022/Day 15/day15.py
+++ 2022/Day 15/day15.py
@@ -1,13 +1,21 @@
 import math
 import re
+from xml.sax.xmlreader import XMLReader
 
-file = open('input.csv', 'r')
+file = open('2022/Day 15/input.csv', 'r')
 lines = file.readlines()
 lines = [line.strip('\n' ',') for line in lines]
 
 
-def manhattan(sensor, beacon):
-    return abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+class Reading:
+    def __init__(self, sensor, beacon):
+        self.sensor = sensor
+        self.beacon = beacon
+        self.distance = manhattan(sensor, beacon)
+
+
+def manhattan(point_a, point_b):
+    return abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1])
 
 
 def get_min(current_min, sensor, beacon):
@@ -20,55 +28,75 @@ def get_min(current_min, sensor, beacon):
 
 
 def get_max(current_max, sensor, beacon):
-    if sensor[0] - manhattan(sensor, beacon) > current_max:
-        current_max = sensor[0] - manhattan(sensor, beacon)
+    if sensor[0] + manhattan(sensor, beacon) > current_max:
+        current_max = sensor[0] + manhattan(sensor, beacon)
     if beacon[0] > current_max:
         current_max = beacon[0]
 
     return current_max
 
 
+def can_have_beacon(readings, test_point):
+    for reading in readings:
+        if reading.beacon[0] == test_point[0] and reading.beacon[1] == test_point[1]:
+            return False
+        if reading.sensor[0] == test_point[0] and reading.sensor[1] == test_point[1]:
+            return False
+
+        dist = reading.distance
+        if manhattan(reading.sensor, test_point) <= dist:
+            return False
+
+    return True
+
+
+def circle_x_range(reading, y):
+    mid = reading.sensor
+    x_min = mid[0] - (reading.distance - abs(mid[1] - y))
+    x_max = mid[0] + (reading.distance - abs(mid[1] - y))
+    return x_min, x_max
+
+
 def part1():
     sensors_beacons = {}
     x_range = [math.inf, -math.inf]
-    y_range = [math.inf, -math.inf]
-
+    readings = []
     for line in lines:
         numbers = [int(x.group()) for x in re.finditer(r'\d+', line)]
         sensor = (numbers[0], numbers[1])
         beacon = (numbers[2], numbers[3])
         sensors_beacons[sensor] = beacon
+        readings.append(Reading(sensor, beacon))
         x_range[0] = get_min(x_range[0], sensor, beacon)
         x_range[1] = get_max(x_range[1], sensor, beacon)
-        y_range[0] = get_min(y_range[0], sensor, beacon)
-        y_range[1] = get_max(y_range[1], sensor, beacon)
 
-    y = 10  # 2000000
-    print(x_range)
-    row = ["." for _ in range(y_range[1]-y_range[0])]
-    beaconless = []
-    beaconless_count = 0
-    for x in range(y_range[0], y_range[1]):
-        possible = True
-        for sensor in sensors_beacons:
-            beacon = sensors_beacons[sensor]
-            if beacon[1] == y:
-                row[beacon[0] + x_range[0]] = "B"
-            dist = manhattan(sensor, beacon)
-            if manhattan(sensor, (y, x)) < dist:
-                possible = False
-                break
-        if possible:
-            beaconless.append((y, x))
-            beaconless_count += 1
-            row[x + x_range[0]] = "."
-        else:
-            row[x + x_range[0]] = "#"
-    print(beaconless_count)
-    print(beaconless)
-    print(row)
+    y = 2000000 # 5 127 797 x5065467 x5065465
+    x_offset = abs(x_range[0])
 
+    for y in range(y, y+1):
+        row = ["." for _ in range(x_range[1]+x_offset+1)]
+    
+        for reading in readings:
+            if reading.beacon[1] == y:
+                row[reading.beacon[0] + x_offset] = "B"
+            elif reading.sensor[1] == y:
+                row[reading.sensor[0] + x_offset] = "S"
+        # print("".join(row))
+        count = 0
 
+        for reading in readings:
+            start_x, end_x = circle_x_range(reading, y)
+            for x in range(start_x, end_x+1):
+                if row[x + x_offset] == "B" or row[x + x_offset] == "S":
+                    continue
+
+                row[x + x_offset] = "#"
+                count += 1
+        #print("".join(row))
+    print(count)
+    print(row.count("S"))
+    print(row.count("#"))
+    return
 
 
 if __name__ == "__main__":
